78aec95296d6a61cb182b67c477999b90619025f
Courtnee McCollum <courtneemeaghan@gmail.com>
2022-03-25 11:36:56 -0500

    Add basic calculator functionality

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const currentCalculation = [];
+const operators = ['+', '-', 'x', '/'];
+
 const calculator = {
   calculate: {
     add: (a, b) => a + b,
@@ -8,15 +11,89 @@ const calculator = {
   operate: function(operator, a, b) {
     switch (operator) {
       case '+':
-        return this.calculate.add(a, b);
+        return this.calculate.add(Number(a), Number(b));
       case '-':
-        return this.calculate.subtract(a, b);
+        return this.calculate.subtract(Number(a), Number(b));
       case 'x':
-        return this.calculate.multiply(a, b);
+        return this.calculate.multiply(Number(a), Number(b));
       case '/':
-        return this.calculate.divide(a, b);
+        return this.calculate.divide(Number(a), Number(b));
     }
   }
 }
 
-console.log(calculator.operate('/', 5, 2));
+
+const buttonEventHandlers = {
+  addButtonListener: () => {
+    const buttons = document.querySelectorAll('button');
+    [...buttons].forEach(btn => btn.addEventListener('click', () => {
+      const calcDisplay = document.querySelector('#calculator-display');
+      let value = btn.textContent;
+      switch (value) {
+        case 'C':
+          while (currentCalculation.length > 0) currentCalculation.pop();
+          break;
+        case 'DEL':
+          if (currentCalculation.length > 0) {
+            let modifiedValue = currentCalculation.pop();
+            
+            if (modifiedValue.length > 1) {
+              modifiedValue = modifiedValue.split('');
+              modifiedValue.pop();
+            } else {
+              modifiedValue = '';
+            }
+            
+            if (modifiedValue) currentCalculation.push(modifiedValue.join(''));
+          }
+          break;
+        case '0':
+        case '1':
+        case '2':
+        case '3':
+        case '4':
+        case '5':
+        case '6':
+        case '7':
+        case '8':
+        case '9':
+          if (currentCalculation.length === 0 || operators.includes(currentCalculation[currentCalculation.length - 1])) {
+            currentCalculation.push(value);
+          } else {
+            currentCalculation[currentCalculation.length - 1] += value;
+          }
+          break;
+        case '+':
+        case '-':
+        case 'x':
+        case '/':
+          currentCalculation.forEach(el => {
+            if (operators.includes(el)) {
+              let newCalculation = calculator.operate(el, currentCalculation[0], currentCalculation[currentCalculation.length - 1]);
+
+              while (currentCalculation.length > 0) currentCalculation.pop();
+              currentCalculation.push(newCalculation, value);
+            }
+          });
+          
+          if (operators.includes(value) && currentCalculation[currentCalculation.length - 1] === String(Number(currentCalculation[currentCalculation.length - 1]))) {
+            currentCalculation.push(value);
+          }
+          break;
+        case '=':
+          if (currentCalculation.length === 3) {
+            let [operand1, operator, operand2] = currentCalculation;
+            let newCalculation = calculator.operate(operator, operand1, operand2);
+
+            while (currentCalculation.length > 0) currentCalculation.pop();
+            currentCalculation.push(String(newCalculation));
+          }
+      }
+
+      calcDisplay.textContent = currentCalculation.join(' ');
+      if (currentCalculation.length === 0) calcDisplay.textContent = '0';
+    }));
+  },
+}
+
+buttonEventHandlers.addButtonListener();
